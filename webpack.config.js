e4d1ae7d632dc546e73bcd939a16b0b7cd52f87d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2023-03-22 06:01:06 -0600

    Configuración de Webpack y Babel

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: { minimize: true },
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+	],
+};
